4aacfde2c65ac267724636f225c5ec7762ce7974
arjun aggarwal <arjun.2003.mail@gmail.com>
2022-12-22 22:41:15 -0500

    part two

---
+++ day_two/strategy_b.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <map>
+
+using namespace std;
+
+int main() {
+    ifstream file ("rps.txt");
+    string line;
+    int points;
+    map<char, char> wins;
+    map<char, char> draws;
+    map<char, char> losses;
+    map<char, int> values;
+
+    wins['A'] = 'P'; // paper beats their rock
+    wins['B'] = 'S'; // scissors beats their paper
+    wins['C'] = 'R'; // rock beats their scissors
+
+    draws['A'] = 'R'; // rock draws with rock
+    draws['B'] = 'P'; // paper draws with paper
+    draws['C'] = 'S'; // scissors draws with scissors
+
+    losses['A'] = 'S'; // scissors loses to their rock
+    losses['B'] = 'R'; // rock loses to their paper
+    losses['C'] = 'P'; // paper loses to their scissors
+
+    values['R'] = 1;
+    values['P'] = 2;
+    values['S'] = 3;
+
+    points = 0;
+    if (file.is_open()) {
+        while(getline(file, line)) {
+            char their_move = line[0];
+            char outcome = line[2];
+
+            if (outcome == 'X') {
+                points += values[losses[their_move]];
+            }
+            else if (outcome == 'Y') {
+                points += 1;
+                points += values[draws[their_move]];
+            }
+            else {
+                points += 3;
+                points += values[wins[their_move]];
+            }
+        }
+    }
+
+    cout << points << "\n";
+}
