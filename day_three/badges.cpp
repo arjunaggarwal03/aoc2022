301bc6747a3fa2d86ceb5132acea2646d8482826
arjun aggarwal <arjunaggarwal173@gmail.com>
2022-12-24 07:02:42 -0500

    brute force

--- day_three/badges.cpp
+++ day_three/badges.cpp
@@ -11,20 +11,26 @@ using namespace std;
 
 int main() {
     ifstream file ("rucksacks.txt");
-    string line;
-    vector<char> repeats;
+    string sack_a, sack_b, sack_c;
+    vector<char> badges;
 
     if (file.is_open()) {
-        while(getline(file, line)) {
-            string line_b, line_c;
-            getline(file, line_b);
-            getline(file, line_c);
-            cout << line << line_b << line_c << "\n";
+        while(getline(file, sack_a)) {
+            getline(file, sack_b);
+            getline(file, sack_c);
+            
+            for (char item : sack_a) {
+                if (find(sack_b.begin(), sack_b.end(), item) != sack_b.end() &&
+                    find(sack_c.begin(), sack_c.end(), item) != sack_c.end()) {
+                    badges.push_back(item);
+                    break;
+                }
+            }
         }
     }
 
     int total_priority = 0;
-    for (char item : repeats) {
+    for (char item : badges) {
         if (isupper(item)) {
             total_priority += int(item) - 38;
         }
