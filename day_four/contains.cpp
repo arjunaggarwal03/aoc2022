db6ad81212367306fb14755796c8f80ad48ac322
arjun aggarwal <arjunaggarwal173@gmail.com>
2022-12-25 19:03:02 -0500

    added part two
    renamed overlaps to contains

---
+++ day_four/contains.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstring>
+#include <cctype>
+#include <vector>
+#include <map>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ifstream file ("assignments.txt");
+    string line;
+    int overlaps = 0;
+
+    if (file.is_open()) {
+        while(getline(file, line)) {
+            string assignment_a = line.substr(0, line.find(","));
+            string assignment_b = line.substr(line.find(",") + 1, line.size() - line.find(","));
+            int start_a = stoi(assignment_a.substr(0, assignment_a.find("-")));
+            int end_a = stoi(assignment_a.substr(assignment_a.find("-") + 1, assignment_a.size() - assignment_a.find("-")));
+            int start_b = stoi(assignment_b.substr(0, assignment_b.find("-")));
+            int end_b = stoi(assignment_b.substr(assignment_b.find("-") + 1, assignment_b.size() - assignment_b.find("-")));
+            
+            if (start_a <= start_b && end_a >= end_b) {
+                overlaps++;
+            }
+            else if (start_b <= start_a && end_b >= end_a) {
+                overlaps++;
+            }
+        }
+    }
+
+    cout << overlaps << "\n";
+}
